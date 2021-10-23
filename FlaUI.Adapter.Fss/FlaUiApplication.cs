87fa8492429b2d74e090b7d0a20f703ae164cebd
Tom Wells <twells43@comcast.net>
2021-10-23 00:37:09 -0700

    * FlaUI.Adapters.Fss

---
+++ FlaUI.Adapter.Fss/FlaUiApplication.cs
@@ -0,0 +1,36 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+using System;
+using System.Diagnostics;
+
+namespace FlaUI.Adapter.Fss
+{
+    public class FlaUiApplication
+    {
+        private Application _application;
+        private UIA3Automation _automation;
+        private ConditionFactory _by;
+        private Window _mainWindow;
+
+        public FlaUiApplication(ProcessStartInfo processStartInfo, TimeSpan applicationLaunchWaitTime)
+        {
+            _application = FlaUI.Core.Application.AttachOrLaunch(processStartInfo);
+            _application.WaitWhileMainHandleIsMissing(applicationLaunchWaitTime);
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _automation = new UIA3Automation();
+            _by = new ConditionFactory(new UIA3PropertyLibrary());
+            _mainWindow = _application.GetMainWindow(_automation);
+        }
+
+        public Application Application => _application;
+        public UIA3Automation Automation => _automation;
+        public ConditionFactory By => _by;
+        public Window MainWindow => _mainWindow;
+    }
+}
---
+++ Tests/Fss.FlaUI.NUnitTests/Tests1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fss.FlaUI.NUnitTests
+{
+    [TestFixture]
+    public class Tests1
+    {
+        [Test]
+        public void GarbageTest01()
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+}
+
